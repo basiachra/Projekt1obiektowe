5f5085eba8879da11e98959a94f69e0258f2eb38
basiachra <basia.chra@gmail.com>
2016-12-06 23:32:13 +0100

    Add files via upload

--- src/Chapter.java
+++ src/Chapter.java
@@ -1,15 +1,61 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class Chapter {
-
-	public void czytajrozdzial(int x[])
-	{	
-		//wyszukanie rozdzialow i wypisanie tytulu
-		// gdy jest przedzial to petla for
-		//wywolanie funkcji czytajartykul 
-		//klasa scanner
 	
+		public String line;
+		public String path;
+		
+		public Chapter(String file_path){
+			path = file_path;
+		}		
+		
+		public void readchapter(String x, String y)
+		{			
+		try (Scanner in = new Scanner(new File(path)))
+		{
+			line = in.nextLine();				
+				
+			while(!line.equals(null))
+			{		
+				if(line.equals("Rozdzia³ " + x))
+				{
+					while(!line.equals("Rozdzia³ " + y))
+					{			
+						
+						if(chapunnecessaryelemetnt())
+						{
+							if(!line.endsWith("-")) { System.out.println(line); }
+							else join(in);
+						}
+						line = in.nextLine();
+					}
+				}
+				line = in.nextLine();
+			}
+		} catch(IOException exc){ System.out.println("B³¹d: " + exc); }
 	}
+			
+	
+	public boolean chapunnecessaryelemetnt()
+	{
+		if(line.equals("©Kancelaria Sejmu") || line.equals("2009-11-16"))		
+			return false;			
+		return true;		
+	}
+
 	
 	
+	public void join(Scanner in)
+	{			
+		line = line.substring(0, line.length() - 1) + in.nextLine();
+		if(line.endsWith("-"))
+		{
+			join(in);
+		}		
+		else
+		System.out.println(line);
+		
+	}
 	
 }
