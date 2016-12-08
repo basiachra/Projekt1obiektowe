d27d351a559cd72054efcc043010062dd1614144
basiachra <basia.chra@gmail.com>
2016-12-08 23:05:20 +0100

    Add files via upload

--- src/ArgumentParser.java
+++ src/ArgumentParser.java
@@ -5,7 +5,7 @@ public class ArgumentParser {
 	public static void article(int arg1, int arg2 , String path) throws IOException
 	{	
 		
-		if(arg1<1 || arg1>243 || arg2<arg1 || arg2>243 ) System.out.println("Podaj poprawny zakres");
+		if(arg1<1 || arg1>243 || arg2<arg1 || arg2>243 ) System.out.println("Niepoprawny zakres");
 		else Article.readarticle(arg1, arg2, path);
 	
 	}	
